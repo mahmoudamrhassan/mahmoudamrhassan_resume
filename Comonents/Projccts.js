4a67439557647e18e6805cb72482c5aa5552afff
mahmoudamr5896 <mahmoudamr5896@gmail.com>
2025-04-29 22:02:34 +0300

    up

--- Comonents/Projccts.js
+++ Comonents/Projccts.js
@@ -258,7 +258,7 @@ const projects = [
     title: 'E-commerce Application',
     description: 'A comprehensive e-commerce solution built with Js , HTML, and CSS.',
     image: '/imges/ecommerce1.png',
-    livePreview: '#',
+    livePreview: 'http://127.0.0.1:5502/home/homepage.html',
     github: 'https://github.com/mahmoudamr5896/',
     category: 'Full Stack Developer',
 
